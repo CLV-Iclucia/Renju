ce14a38b88e6c0b07f5989e12ab69fa411a92c2c
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2022-10-14 08:56:30 +0800

    Start to use CUDA

--- main.cu
+++ main.cu
@@ -1,8 +1,10 @@
-#include <stdio.h>
-#include <math.h>
-#include <limits.h>
-#include "GamePlay.h"
-#include "utils.h"
+extern "C"
+{
+    #include <stdio.h>
+    #include "GamePlay.h"
+    #include "utils.h"
+}
+
 const int CLR[15] = {~3, ~(3 << 2), ~(3 << 4), ~(3 << 6), ~(3 << 8), ~(3 << 10), ~(3 << 12), ~(3 << 14), ~(3 << 16), ~(3 << 18), ~(3 << 20),
          ~(3 << 22), ~(3 << 24), ~(3 << 26), ~(3 << 28)};//use these constants to clear the board
 int row[SIZE], col[SIZE], diagL[SIZE<<1], diagR[SIZE<<1];//00 stands for no chess, 10 stands for black, 11 stands for white.So I can use "COLOR^=1" to change color.
